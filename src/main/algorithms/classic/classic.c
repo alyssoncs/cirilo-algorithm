eb1960c0495403f2b54bd3f812e256da0d4aa85a
alysson <alysson.cirilo@gmail.com>
2024-07-31 09:25:11 -0300

    refactor classic algorithm

--- src/main/algorithms/classic/classic.c
+++ src/main/algorithms/classic/classic.c
@@ -5,7 +5,7 @@ static long diff(long a, long b)
 	return labs(a - b);
 }
 
-static int cmp (const void * a, const void * b) 
+static int cmp(const void * a, const void * b) 
 {
 	return (*(int *)a - *(int *)b);
 }
@@ -15,27 +15,24 @@ int *classic_find(int first_array[], int second_array[], int m, int n)
 	qsort(first_array, m, sizeof(int), cmp); 
 	qsort(second_array, n, sizeof(int), cmp); 
   
-	int a = 0, b = 0; 
-  
 	int *closest_pair = malloc(sizeof(int) * 2);
-
-	if (!closest_pair) {
+	if (!closest_pair)
 		return NULL;
-	}
 	
 	closest_pair[0] = first_array[0];
 	closest_pair[1] = second_array[0];
   
-	while (a < m && b < n) { 
-		if (diff(first_array[a], second_array[b]) < diff(closest_pair[0], closest_pair[1])) {
-				closest_pair[0] = first_array[a];
-				closest_pair[1] = second_array[b];
+	int i = 0, j = 0; 
+	while (i < m && j < n) { 
+		if (diff(first_array[i], second_array[j]) < diff(closest_pair[0], closest_pair[1])) {
+			closest_pair[0] = first_array[i];
+			closest_pair[1] = second_array[j];
 		}
   
-		if (first_array[a] < second_array[b]) 
-			a++; 
+		if (first_array[i] < second_array[j]) 
+			i++; 
 		else
-			b++; 
+			j++; 
 	} 
   
 	return closest_pair;  
